a0f6bd83e9a3d81fb943c06e7138218b9bcc567b
Aly <aly.kazani52@gmail.com>
2026-01-22 14:39:05 +0500

    Join local and origin repos

---
+++ Lab1/hr.sql
@@ -0,0 +1,16 @@
+-- 1
+--select * from jobs;
+
+-- 2
+--select min_salary, max_salary from jobs
+--where JOB_TITLE = 'President';
+
+-- 3
+--select * from employees 
+--where  SALARY > 20000;
+
+-- 4
+--select job_id from employees
+--where  salary >= 15000;
+
+-- 5
